00456a26537b1a15bfaf1df2053f51b26c6807e6
Roberts Briņķis <60570885+amixaam@users.noreply.github.com>
2024-12-08 16:09:35 +0200

    update script

--- newproject.sh
+++ newproject.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This script runs the neccessary commands to setup the project when cloning it from the repo
 # Run from the project root
 
@@ -7,7 +9,7 @@ composer install
 npm install
 
 php artisan key:generate
-php artisan migrate:fresh --seed
+php artisan migrate --seed
 
 echo "Done! You can now run 'composer run dev' to start developing."
 echo "Make sure to change the values in .env to your own."
